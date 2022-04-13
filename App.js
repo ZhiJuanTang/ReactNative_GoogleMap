c46463d64a8bd3479baafcdb69792be5ae23fdde
ZhiJuanTang <tangzhijuan56@gmail.com>
2022-04-13 19:17:24 +0200

    change the wrong  app to this supposed one

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import FreeNowCars from "./components/FreeNowCars";
+
+const fakeDriverLocations = [
+  {
+    'id': 1,
+    "coordinate": {
+      "latitude": 53.5514746,
+      "longitude": 10.0031117
+  },
+},
+  {
+    'id': 2,
+    "coordinate": {
+      "latitude": 53.55342888743947,
+      "longitude": 10.007842183122419
+  },
+  },
+  {
+    'id': 3,
+    "coordinate": {
+      "latitude": 53.58222524795621,
+      "longitude": 10.066394607153665
+  },
+  },
+];
+
+console.log(fakeDriverLocations);
 
 export default function App() {
+  const [cars, setCars] = useState(fakeDriverLocations);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        initialRegion={{
+          latitude: 53.5532316,
+          longitude: 10.0087783,
+          longitudeDelta: 0.045,
+          latitudeDelta: 0.045,
+        }}
+        style={styles.mapView}
+      >
+        {cars.map((object) => (
+          <FreeNowCars
+            key={object.id}
+            uid={object.id}
+            location={{
+              latitude: object.coordinate.latitude,
+              longitude: object.coordinate.longitude,
+            }}
+          />
+        ))}
+      </MapView>
+      <Text>FreeNowCars </Text>
     </View>
   );
 }
@@ -13,8 +61,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  mapView: {
+    flex: 1,
   },
 });
